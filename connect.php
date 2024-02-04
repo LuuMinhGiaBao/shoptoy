62ec13d81467f37fc802f434c1ffde84174b4cf3
Baolmggcc <baolmggcc210258@fpt.edu.vn>
2024-02-04 20:48:59 +0700

    update2

--- connect.php
+++ connect.php
@@ -6,10 +6,10 @@ class connect{
 	public $password;
 
 	public function __construct(){
-	 $this->server = "grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-	 $this->usernames ="subwb6sap8rdtdvc";
-	 $this->password ="jwkiurq7vim8uc6u";
-	 $this->dbname ="mdgfmx7yyn978en1";	
+	 $this->server = "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+	 $this->usernames ="maukc3m83sdn6l3b";
+	 $this->password ="x4tf04zc33tbyogg";
+	 $this->dbname ="w7mbopsjybsotv16";	
 	}	
 	//1 option:1
 	
